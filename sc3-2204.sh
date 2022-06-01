c30a3b684da08ed9a69f5ff0ec90c5bfd4589b8f
Your Name <you@example.com>
2022-06-01 11:06:37 +0300

    docker install improvement

--- sc3-2204.sh
+++ sc3-2204.sh
@@ -39,7 +39,7 @@ if [ "$exitstatus" != 0 ];  then
 fi
 # Common libs install
 #apt-install "software-properties-common apt-transport-https net-tools openssh-server"
-apt-install "software-properties-common apt-transport-https net-tools ca-certificates curl"
+sudo apt-install "software-properties-common apt-transport-https net-tools ca-certificates curl gnupg lsb-release"
 sudo apt --fix-broken install
 for item in $LIST
 do
@@ -118,11 +118,13 @@ case $item in
 	
 	"11") # Docker
 	msg "Docker for Ubuntu stable release"
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt update
 	apt-cache policy docker-ce
-	sudo apt install "docker-ce docker-compose"
+	apt-install "docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose"
 	sudo usermod -aG docker ${USER}
 	su - ${USER}
 	id -nG
