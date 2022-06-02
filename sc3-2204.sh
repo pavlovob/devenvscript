1dc5c6946af15ef54106859b355a06a1a5baf259
Your Name <you@example.com>
2022-06-02 14:49:38 +0300

    terraform changed

--- sc3-2204.sh
+++ sc3-2204.sh
@@ -138,7 +138,7 @@ case $item in
 	"13") # Terraform
 	msg "Hashicorp Terraform for Ubuntu stable release"
 	curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-	sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+	sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 	apt-install "terraform"
 	;;
 
