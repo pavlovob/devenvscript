0cd4fd2b18a9cd21b45a1b397359d881b302f4e6
Your Name <you@example.com>
2022-06-14 09:25:30 +0300

    !

--- main.sh
+++ main.sh
@@ -118,12 +118,22 @@ case $item in
 	
 	"11") # Docker
 	msg "Docker for Ubuntu stable release"
-	# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	#Digital ocean method:
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt update
-	apt-cache policy docker-ce
+
+	#sudo mkdir -p /etc/apt/keyrings
+ 	#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	#echo \
+  	#"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ 	#$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	# sudo apt update
+	# apt-cache policy docker-ce
+	#sudo chmod a+r /etc/apt/keyrings/docker.gpg
 	apt-install "docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose"
 	sudo usermod -aG docker ${USER}
 	su - ${USER}
